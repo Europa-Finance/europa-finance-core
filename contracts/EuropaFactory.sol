8dd70c47f8dbc39670b169a447f3701bb1abb12a
thekid-lestrange <thekid.lestrange@gmail.com>
2021-07-30 14:12:20 +0700

    europa code update

--- contracts/EuropaFactory.sol
+++ contracts/EuropaFactory.sol
@@ -1,10 +1,10 @@
 pragma solidity =0.5.16;
 
-import './interfaces/IPancakeFactory.sol';
-import './PancakePair.sol';
+import './interfaces/IEuropaFactory.sol';
+import './EuropaPair.sol';
 
-contract PancakeFactory is IPancakeFactory {
-    bytes32 public constant INIT_CODE_PAIR_HASH = keccak256(abi.encodePacked(type(PancakePair).creationCode));
+contract EuropaFactory is IEuropaFactory {
+    bytes32 public constant INIT_CODE_PAIR_HASH = keccak256(abi.encodePacked(type(EuropaPair).creationCode));
 
     address public feeTo;
     address public feeToSetter;
@@ -12,27 +12,27 @@ contract PancakeFactory is IPancakeFactory {
     mapping(address => mapping(address => address)) public getPair;
     address[] public allPairs;
 
-    event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint256);
 
     constructor(address _feeToSetter) public {
         feeToSetter = _feeToSetter;
     }
 
-    function allPairsLength() external view returns (uint) {
+    function allPairsLength() external view returns (uint256) {
         return allPairs.length;
     }
 
     function createPair(address tokenA, address tokenB) external returns (address pair) {
-        require(tokenA != tokenB, 'Pancake: IDENTICAL_ADDRESSES');
+        require(tokenA != tokenB, 'Europa: IDENTICAL_ADDRESSES');
         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-        require(token0 != address(0), 'Pancake: ZERO_ADDRESS');
-        require(getPair[token0][token1] == address(0), 'Pancake: PAIR_EXISTS'); // single check is sufficient
-        bytes memory bytecode = type(PancakePair).creationCode;
+        require(token0 != address(0), 'Europa: ZERO_ADDRESS');
+        require(getPair[token0][token1] == address(0), 'Europa: PAIR_EXISTS'); // single check is sufficient
+        bytes memory bytecode = type(EuropaPair).creationCode;
         bytes32 salt = keccak256(abi.encodePacked(token0, token1));
         assembly {
             pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
         }
-        IPancakePair(pair).initialize(token0, token1);
+        IEuropaPair(pair).initialize(token0, token1);
         getPair[token0][token1] = pair;
         getPair[token1][token0] = pair; // populate mapping in the reverse direction
         allPairs.push(pair);
@@ -40,12 +40,12 @@ contract PancakeFactory is IPancakeFactory {
     }
 
     function setFeeTo(address _feeTo) external {
-        require(msg.sender == feeToSetter, 'Pancake: FORBIDDEN');
+        require(msg.sender == feeToSetter, 'Europa: FORBIDDEN');
         feeTo = _feeTo;
     }
 
     function setFeeToSetter(address _feeToSetter) external {
-        require(msg.sender == feeToSetter, 'Pancake: FORBIDDEN');
+        require(msg.sender == feeToSetter, 'Europa: FORBIDDEN');
         feeToSetter = _feeToSetter;
     }
 }
